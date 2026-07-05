6df785df6d2f3e6c9ea707ee079cee85e462adf5
Matheus DEV <matheusalmeida.devv@gmail.com>
2026-07-05 12:57:44 -0300

    Aprendendo JavaScript - Closures (Praticando mais sobre)

---
+++ JavaScript/JSAvancado/PRATICANDO+/Closures2.js
@@ -0,0 +1,33 @@
+//Acredito que eu preciso praticar mais sobre closures
+//por ser o assunto mais diferente que eu ja vi 
+
+/*Crie uma closure chamada criarContador que:
+
+Não recebe parâmetro nenhum
+Retorna um objeto com três funções:
+
+incrementar — soma 1 ao contador
+decrementar — subtrai 1 do contador
+valor — retorna o valor atual do contador
+*/
+
+function criarContador(){
+    let conta = 0;
+    return {
+
+        
+        incrementar: function() {conta ++;},
+        decrementar: function() {conta --;},
+        valor: function() {return conta}
+       
+
+    };
+
+}
+
+const contador = criarContador();
+contador.incrementar();
+contador.incrementar();
+contador.incrementar();
+contador.decrementar();
+console.log(contador.valor()); // 2
