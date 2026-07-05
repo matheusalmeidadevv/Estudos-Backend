7925181d2f7a51fcc16f290ab0fd21feea6bb2ba
Matheus DEV <matheusalmeida.devv@gmail.com>
2026-07-05 12:54:29 -0300

    Aprendendo JavaScript - Closures

--- JavaScript/JSAvancado/Closures.js
+++ JavaScript/JSAvancado/Closures.js
@@ -1,3 +1,11 @@
+
+/* 
+Closures é quando uma função "lembra" e acessa variáveis de um escopo externo,
+ mesmo depois que essa função externa já foi finalizada.
+ No exemplo abaixo, a função interna continua lembrando o valor de "numero"
+ porque ele foi capturado pelo closure quando a função criarSomador foi chamada.
+*/
+
 function criarSomador(numero) {
 
     return function (num){
