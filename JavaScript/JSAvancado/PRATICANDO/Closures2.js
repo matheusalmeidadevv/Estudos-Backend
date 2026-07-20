b88b9b46a2556c96c4268ceaeaeb917d67fba22c
Matheus DEV <matheusalmeida.devv@gmail.com>
2026-07-20 12:03:58 -0300

    chore: Move a Closures2 para a pasta PRATICANDO

--- JavaScript/JSAvancado/PRATICANDO/Closures2.js
+++ JavaScript/JSAvancado/PRATICANDO/Closures2.js
@@ -1,7 +1,7 @@
 /*Crie uma closure chamada criarContador que:
 
 Não recebe parâmetro nenhum
-Retorna um objeto com três funções:
+Retorna um objeto com três funções: 
 
 incrementar — soma 1 ao contador
 decrementar — subtrai 1 do contador
